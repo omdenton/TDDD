967d1bf62d52cfe83061427d3c4ca3eff955359a
Oli Denton <om.denton@gmail.com>
2018-11-07 17:57:14 +0000

    first steps, HW done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str,numRepeats) 
+{
+  let returnValue = "";
 
-}
+  if (numRepeats > 0)
+  {
+    for ( i=0; i <= numRepeats; i++)
+    {
+      returnValue += str;
+    }
+    return returnValue;
+  }
+  else 
+  {
+    return "ERROR";
+  }
+};
 
-module.exports = repeatString
+module.exports = repeatString;
