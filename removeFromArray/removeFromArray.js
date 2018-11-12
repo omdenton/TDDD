50a45c0ad03fb228194d1271de35500fced1e01c
Oli Denton <om.denton@gmail.com>
2018-11-12 22:37:50 +0000

    progress on remove

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
-}
-
-module.exports = removeFromArray
+const removeFromArray = function(arguments) 
+  {
+    let array = [];
+    for (i=0; i < 4; i++) array[i] = arguments[i];
+    for (j=3; j<arguments.length; j++)
+    {
+      for (i=0; i<array.length; i++)
+      {
+        if (array[i] == arguments[j]) array.splice(i,1);
+      }
+    } 
+  
+  return array; 
+  
+};
+let result = removeFromArray([1, 2, 3, 4], 3);
+module.exports = removeFromArray;
