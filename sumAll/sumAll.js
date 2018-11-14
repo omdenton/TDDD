3fa201d3da346553af12f902e8b99874e2503d11
Oli Denton <om.denton@gmail.com>
2018-11-14 18:37:23 +0000

    starting sumAll, 2 tests left

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(val1,val2) {
+  
+ let top; let bottom; let sum = 0;
+ if (isNaN(val1) || isNaN(val2)) return "ERROR";
+ if (val1 < 0  || val2 < 0 ) return "ERROR";
+ if (val1 < val2)  { bottom = val1; top = val2; }
+ else { top = val1; bottom = val2; }
 
-}
 
-module.exports = sumAll
+ for (i = bottom; i <= top; i++)
+ {
+    sum = sum + i;
+ }
+
+ return sum;
+};
+result = sumAll(10,"90");
+module.exports = sumAll;
