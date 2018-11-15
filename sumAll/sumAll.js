0f8ae12dc955466bb4339902d374e3d654e0dd34
Oli Denton <om.denton@gmail.com>
2018-11-15 17:26:30 +0000

    all passing for sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -2,6 +2,7 @@ const sumAll = function(val1,val2) {
   
  let top; let bottom; let sum = 0;
  if (isNaN(val1) || isNaN(val2)) return "ERROR";
+ if (typeof val2 != "number") return "ERROR";
  if (val1 < 0  || val2 < 0 ) return "ERROR";
  if (val1 < val2)  { bottom = val1; top = val2; }
  else { top = val1; bottom = val2; }
